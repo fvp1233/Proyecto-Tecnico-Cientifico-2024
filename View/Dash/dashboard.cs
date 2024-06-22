1cead948cec92dc487d756c149ca3c91a27c5231
Fernando Velasquez <fernadomiguelvelasquezperez@gmail.com>
2024-06-21 22:11:44 -0600

    Se crea la carpeta dashboard

---
+++ View/Dash/dashboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTC2024.View.Dash
+{
+    public partial class dash : Form
+    {
+        public dash()
+        {
+            InitializeComponent();
+        }
+    }
+}
